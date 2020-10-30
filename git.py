0a349aa65ea2c75c3155d22dc746f75b6594462c
cartf15 <cartf15@hotmail.com>
2020-10-29 20:54:12 -0500

    Demo agregado y python configurado

--- git.py
+++ git.py
@@ -1,11 +1,18 @@
 from twilio.rest import Client 
- 
+import os
+
+# account_sid = os.environ['twl_sid']
+# auth_token = os.environ['twl_tkn']
+account_sid = 'AC6340922d81a8aaa23e3b36a2ff420ffb' 
+auth_token = 'f384519126914e186e5e0c938dfb6e2d' 
+
+
 
 client = Client(account_sid, auth_token) 
  
 message = client.messages.create( 
                               from_='whatsapp:+14155238886',  
-                              body='Your Yummy Cupcakes Company order of 1 dozen frosted cupcakes has shipped and should be delivered on July 10, 2019. Details: http://www.yummycupcakes.com/',      
+                              body='Your appointment is coming up on July 21 at 3PM',      
                               to='whatsapp:+573142993448' 
                           ) 
  
