2952cddf49ca9fbf7a5db01420b475ba9ac181db
cartf15 <cartf15@hotmail.com>
2020-11-03 19:48:42 -0500

    web version con chromedriver version

--- Web_version.py
+++ Web_version.py
@@ -1,1 +1,12 @@
+# try :
+#     from selenium import webdriver
+# except NameError as e :
+#     print  (e)
+
+
+from selenium import webdriver
+import chromedriver_binary 
+
+browser = webdriver.Chrome(
+    executable_path ='C:/Program Files/chromedriver/chromedriver.exe')
 
